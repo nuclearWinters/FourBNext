db0e00a31aff5fed3c9802b9536a2330085ada36
nuclearWinters <38480267+nuclearWinters@users.noreply.github.com>
2024-03-04 15:03:52 -0500

    fix image config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
     config.experiments.topLevelAwait = true
     return config
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fourb-imgs.s3.us-west-2.amazonaws.com',
+        port: '',
+      },
+    ],
+  }
 }
